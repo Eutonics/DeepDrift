4a6531772e61c22e32d58253426e9d9b65729002
Alexey <9254652@bk.ru>
2025-12-30 18:37:04 +0300

    __init__.py

--- deepdrift/__init__.py
+++ deepdrift/__init__.py
@@ -1,4 +1,6 @@
 from .monitor import DeepDriftMonitor
+from .observer import ObserverConfig, MonitorState
 from .doctor import diagnose_drift
+from .visualization import plot_drift_profile
 
-__all__ = ["DeepDriftMonitor", "diagnose_drift"]
+__all__ = ["DeepDriftMonitor", "ObserverConfig", "MonitorState", "diagnose_drift", "plot_drift_profile"]
